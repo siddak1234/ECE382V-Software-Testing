53633025ca79473e76561eba8682f028b0b8a165
Siddak Bhatia <siddak1234@gmail.com>
2026-02-05 11:45:11 -0600

    Reorganize repo layout and centralize Gradle

--- HW2/gradlew.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set DIR=%~dp0
-set JAVA_CMD=java
-if "%GRADLE_USER_HOME%"=="" set GRADLE_USER_HOME=%DIR%.gradle-user-home
-if not "%JAVA_HOME%"=="" set JAVA_CMD=%JAVA_HOME%\bin\java
-"%JAVA_CMD%" -classpath "%DIR%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
-endlocal
